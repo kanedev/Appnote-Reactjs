cc3d81ae33f5c92982098bc092daa94c3f3e5594
kanedev <kanedev@live.fr>
2019-06-06 23:51:28 +0200

    Création de deux composant, avec passage de variables

---
+++ src/composant2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+
+function Composant2(props) {
+  return (
+    <div>
+    <p>
+Composant : 2 , props1 = {props.props1}   et props2 = {props.props2} 
+    </p>
+    <p> {props.children}</p>
+  </div>
+  );
+}
+
+export default Composant2;
