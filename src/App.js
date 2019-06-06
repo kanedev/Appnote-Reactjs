f1d493f57631257b5ac863f68ad449eefb83fa1d
kanedev <kanedev@live.fr>
2019-06-06 23:54:54 +0200

    la notion de children dun composant

--- src/App.js
+++ src/App.js
@@ -13,10 +13,9 @@ function App() {
         </p>
 
 
-<Composant2  props1="contenu 1.1" props2="contenu 1.2" >Falso</Composant2>
-<Composant2  props1="contenu 2.1" props2="contenu 2.2" />
-<Composant2  props1="contenu 3.1" props2="contenu 3.2" />
-
+<Composant2  props1="contenu 1.1" props2="contenu 1.2" >Child 1</Composant2>
+<Composant2  props1="contenu 2.1" props2="contenu 2.2"  >Child 2</Composant2>
+<Composant2  props1="contenu 3.1" props2="contenu 3.2"  >Child 3</Composant2>
      
       </header>
     </div>
